23debd6d4b37b1aedc9b2396cea549ce59cf7db3
pragun-bansal <pragun.bansal@yahoo.in>
2026-04-12 16:02:04 +0530

    logo photo

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/images/Logo.png" type="image/png" />
+        </head>
         <body>
         <Providers>
 
